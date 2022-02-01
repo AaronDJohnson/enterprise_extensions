03b691f60d96bce7a12b5112fc9b4f480f7a6f4b
Aaron <Aaron9034@Hotmail.com>
2022-02-01 10:11:42 -0600

    Update chromatic.py

--- enterprise_extensions/chromatic/chromatic.py
+++ enterprise_extensions/chromatic/chromatic.py
@@ -45,7 +45,8 @@ def chrom_exp_decay(toas, freqs, log10_Amp=-7, sign_param=-1.0,
     # ind = jnp.where(toas > t0)[0]
     ind = jnp.searchsorted(toas, t0)
     wf = 10**log10_Amp * jnp.heaviside(toas - t0, 1)
-    wf = wf.at[ind:].set(wf[ind:] * jnp.exp(- (toas[ind:] - t0) / tau))
+    for ii in range(ind, len(toas)):
+        wf = wf.at[ii].set(wf[ii] * jnp.exp(- (toas[ii] - t0) / tau))
 
     return jnp.sign(sign_param) * wf * (1400 / freqs) ** idx
 
